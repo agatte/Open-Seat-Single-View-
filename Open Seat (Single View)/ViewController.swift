37066044db052d9d8040f03c42d30491308b290e
Anthony <gattea@xavier.edu>
2018-11-16 04:09:11 -0500

    Initial Commit

---
+++ Open Seat (Single View)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Open Seat (Single View)
+//
+//  Created by Anthony Gatte on 11/16/18.
+//  Copyright © 2018 Anthony Gatte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
